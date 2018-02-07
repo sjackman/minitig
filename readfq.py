8d3be8f3a19ef69641195ac91acf2f13d93d3b9c
Shaun Jackman <sjackman@gmail.com>
2018-02-06 17:06:41 -0800

    readfq.py: Replace readfq with read_fasta

--- readfq.py
+++ readfq.py
@@ -1,4 +1,5 @@
-def readfq(fp): # this is a generator function
+def read_fasta(fp): # this is a generator function
+    "See https://github.com/lh3/readfq"
     last = None # this is a buffer keeping the last unprocessed line
     while True: # mimic closure; is it a bad idea?
         if not last: # the first record or a record following a fastq
@@ -14,26 +15,16 @@ def readfq(fp): # this is a generator function
                 break
             seqs.append(l[:-1])
         if not last or last[0] != '+': # this is a fasta record
-            yield name, ''.join(seqs), None # yield a fasta record
+            yield name, ''.join(seqs) # yield a fasta record
             if not last: break
         else: # this is a fastq record
-            seq, leng, seqs = ''.join(seqs), 0, []
+            seq, leng = ''.join(seqs), 0
             for l in fp: # read the quality
-                seqs.append(l[:-1])
                 leng += len(l) - 1
                 if leng >= len(seq): # have read enough quality
                     last = None
-                    yield name, seq, ''.join(seqs); # yield a fastq record
+                    yield name, seq # yield a fastq record
                     break
             if last: # reach EOF before reading enough quality
-                yield name, seq, None # yield a fasta record instead
+                yield name, seq # yield a fasta record instead
                 break
-
-if __name__ == "__main__":
-    import sys
-    n, slen, qlen = 0, 0, 0
-    for name, seq, qual in readfq(sys.stdin):
-        n += 1
-        slen += len(seq)
-        qlen += qual and len(qual) or 0
-    print n, '\t', slen, '\t', qlen
